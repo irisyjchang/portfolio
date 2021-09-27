95cfdc6fdf8db8cce014d8fd4d384a4bb3186ed3
irisyjchang <irisyjchang@gmail.com>
2021-09-26 21:00:40 -0700

    update

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,3 +1,5 @@
+// Guessing Game By Iris Chang 9/26/21
+// Computer will pick a random number between 0 and 100 inclusive and the user will guess a number. The computer will tell the user if the guess was too high, too low, or correct. If the guess is correct, the computer will display the number of guesses it took the user and if the user would like to play again.
 include<iostream>
 using namespace std;
 
@@ -6,12 +8,14 @@ int main(){
   bool option = true;
   while (option == true){
 
+    // Choose random number between 0 and 100 inclusive.
     srand(time(NULL));
     int randomNum = rand()%101;
 
     int guessNum;
     bool attempt = false;
     int count = 0;
+    // Check user's guess
     while (attempt == false){
       cin >> guessNum;
       count += 1;
@@ -24,11 +28,13 @@ int main(){
       }
     }
 
+    // Output results
     cout << "attempts: " << count;
     cout << "\nplay again ? (y/n)\n";
     char answer;
     cin >> answer;
 
+    // Check if user wants to play again
     if (answer == 'y'){
       option = true;
     } else {
