f1b3b999d164fbe387eb6895dc9f7967c0003d0c
irisyjchang <irisyjchang@gmail.com>
2021-10-10 13:55:27 -0700

    added comments

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,6 +1,7 @@
+// Palindrome by Iris Chang ~ 10/10/21
 #include<iostream>
 using namespace std;
-
+// Check for punctuation
 bool punctuation(char forwardLetter){
   if (forwardLetter == '.' || forwardLetter == ' ' || forwardLetter == ',' || forwardLetter == ';' || forwardLetter == ':'|| forwardLetter == '!' || forwardLetter == '?'){
     return true;
@@ -18,28 +19,31 @@ int main(){
   int tally = 0;
 
   cin >> noskipws;
+  // Check user input
   while (true){
     cin >> forwardLetter;
     if (punctuation(forwardLetter)){
       continue;
     }
+    // Terminate user input
     if (forwardLetter == '\n'){
       break;
     }else{
       count++;
       forwardInput[index]=forwardLetter;
       index++;
+      // Check user input size
       if (index == 81){
 	cout << "error : not enough storage";
         break;
       }
     }
   }
- 
+  // Forming array that is the backward version of user input
   for (int i=0; i<index; i++){
     backwardInput[index-i-1] = forwardInput[i];
   }
-
+  // Check if user input is same backwards as forwards
   for (int i=0; i<index; i++){
     char backwardLetter = tolower(backwardInput[i]);
     char forwardLetter = tolower(forwardInput[i]);
@@ -47,6 +51,7 @@ int main(){
       tally++;
     }
   }
+  // Output
   if (tally == count){
     cout << "palindrome\n";
   } else {
