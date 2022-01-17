341c798e8022d31af3db30b908bddd44a260f163
irisyjchang <irisyjchang@gmail.com>
2022-01-16 22:16:42 -0800

    new edits

---
+++ LinkedListsPart1/Node.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Student.h"
+
+class Node{
+ public:
+  Node* getNext();
+  Student* getStudent();
+  void setNext(Node*);
+  
+  Node(Student*);
+  ~Node();
+  
+ private:
+  Student* student;
+  Node* next;
+}
--- LinkedListsPart1/Student.h
+++ LinkedListsPart1/Student.h
@@ -1,7 +1,9 @@
-#include
+#pragma once
 using namespace std;
-
 class Student{
  public:
-  
-}
+  char firstName[20];
+  char lastName[10];
+  int id;
+  float gpa;
+};
