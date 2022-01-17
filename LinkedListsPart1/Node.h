2bfc99add4ace7023ccba988d8dbe26b162ee34d
irisyjchang <irisyjchang@gmail.com>
2022-01-16 23:32:41 -0800

    done

--- LinkedListsPart1/Node.h
+++ LinkedListsPart1/Node.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "Student.h"
-
+// Iris Chang 1/16/22 Linked Lists Part 1 : First part of student list program that uses linked lists & shared with another partner
 class Node{
  public:
   Node* getNext();
   Student* getStudent();
   void setNext(Node*);
-  
+  // constructor & deconstructor
   Node(Student*);
   ~Node();
   
