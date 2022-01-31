614eddb78b0f2826238d54384234a2355cf8dbc2
irisyjchang <irisyjchang@gmail.com>
2022-01-30 19:24:17 -0800

    finished add

--- LinkedListsPart2/Main.cpp
+++ LinkedListsPart2/Main.cpp
@@ -3,18 +3,31 @@
 
 using namespace std;
 
-Node* addNode()
+void addNode(Node* currentNode, Node* node){
+  if (currentNode->getNext() == NULL){
+    // at the end of the linklist
+    currentNode->setNext(node);
+  }else if(currentNode->getNext()->getStudent()->id >= node->getStudent()->id){
+    // insert in front
+    node->setNext(currentNode->getNext());
+    currentNode->setNext(node);
+  }else{
+    addNode(currentNode->getNext(), node);
+  }
+  
+}
+Node* allocateNode()
  {
    Student* student = new Student();
-   cout << "First Name: " << endl;
+   cout << "First Name: ";
    cin >> student->firstName;
    cin.ignore();
-   cout << "Last Name: " << endl;
+   cout << "Last Name: ";
    cin >> student->lastName;
    cin.ignore();
-   cout << "ID: " << endl;
+   cout << "ID: ";
    cin >> student->id;
-   cout << "GPA: " << endl;
+   cout << "GPA: ";
    cin.ignore();
    cin >> student->gpa;
    cin.ignore();
@@ -25,9 +38,9 @@ Node* addNode()
  void print(Node* node)
 {
   if (node != NULL){
-      	cout << node->getStudent()->firstName << endl;
-	cout << node->getStudent()->lastName << endl;
-	cout << node->getStudent()->id << endl;
+      	cout << node->getStudent()->firstName << " ";
+	cout << node->getStudent()->lastName << " ";
+	cout << node->getStudent()->id << " ";
 	cout << node->getStudent()->gpa << endl;
 	print (node->getNext());
       }
@@ -44,9 +57,21 @@ int main(){
       break;
     }else if (strcmp(input, "ADD") == 0){
       Node* node;
-      node = addNode();
-      node->setNext(head);
-      head = node;
+      node = allocateNode();
+      if (head == NULL){
+	head = node;
+      }else if (head->getNext()==NULL){
+	if (head->getStudent()->id >= node->getStudent()->id){
+	  // insert before head
+	  node->setNext(head);
+	  head=node;
+	}else{
+	  // insert after head
+	  head->setNext(node);
+	}
+      }else{
+	addNode(head, node);
+      }
   }else if (strcmp(input, "PRINT") == 0){
       print(head);      
 }else if (strcmp(input, "AVERAGE") == 0){
