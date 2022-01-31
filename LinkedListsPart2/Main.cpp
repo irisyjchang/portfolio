16b00c4fce67cb12a550a739b7b4794d5748f79d
irisyjchang <irisyjchang@gmail.com>
2022-01-30 18:25:23 -0800

    changes

--- LinkedListsPart2/Main.cpp
+++ LinkedListsPart2/Main.cpp
@@ -3,66 +3,56 @@
 
 using namespace std;
 
- void add(Node* head, int newvalue);
- void copyHead (Node* oldHead, Node* newHead);
-
-void addNode (Node** head, Node* node)
-{
-    Node* current = *head;
-    if (current == NULL){
-      *head = node;
-    }
-    else{
-      while (current->getNext() != NULL){
-        current = current->getNext();
-      }
-      current->setNext(node);
-    }
-}
-
-Node* add ()
+Node* addNode()
  {
    Student* student = new Student();
    cout << "First Name: " << endl;
    cin >> student->firstName;
+   cin.ignore();
    cout << "Last Name: " << endl;
    cin >> student->lastName;
+   cin.ignore();
    cout << "ID: " << endl;
    cin >> student->id;
    cout << "GPA: " << endl;
+   cin.ignore();
    cin >> student->gpa;
+   cin.ignore();
    Node* node = new Node(student);
-   node->setNext(NULL);
    return node;
   }
 
- void print(Node* head)
+ void print(Node* node)
 {
-    Node* node = head;
-      while (node != NULL)
-      {
-	cout << node->getStudent()->firstName << endl;
+  if (node != NULL){
+      	cout << node->getStudent()->firstName << endl;
 	cout << node->getStudent()->lastName << endl;
 	cout << node->getStudent()->id << endl;
 	cout << node->getStudent()->gpa << endl;
-        node = node->getNext();
+	print (node->getNext());
       }
 }
 
 int main(){
-  input char[10];
-  while (True){
-    cin >> input;    
+  Node* head = NULL;
+  char input[10];
+  while (true){
+    cout << "QUIT/ADD/PRINT/AVERAGE" << endl;
+    cin >> input;
+    cin.ignore();
     if (strcmp (input, "QUIT") == 0){
       break;
-    }else if ((input, "ADD") == 0){
+    }else if (strcmp(input, "ADD") == 0){
       Node* node;
-      node = add();
+      node = addNode();
       node->setNext(head);
       head = node;
-    }else if ((input, "PRINT") == 0){
+  }else if (strcmp(input, "PRINT") == 0){
       print(head);      
-    }else if ((input, "AVERAGE") == 0){
+}else if (strcmp(input, "AVERAGE") == 0){
+    }else{
+      cout << "ERROR" << endl;
     }
+}
   return 0;
 }
